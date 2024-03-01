48fd7a1d58350c3f2bd9d2a723bcaf2891218dc1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-02 02:17:39 +0300

    proje düzenlend...

--- NTierArchitecture.Entities/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿using NTierArchitecture.Entities.Abstractions;
-
-namespace NTierArchitecture.Entities.Models;
-
-public sealed class Category: Entity
-{
-    public string Name { get; set; }
-    public ICollection<Product> Products { get; set;}
-}
