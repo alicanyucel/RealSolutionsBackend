d00587eb8467c03603e1e6f1296af18066203a06
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-02 02:59:20 +0300

    base entity class genişletildi.dfdsfdfdfdfuu

--- NTierArchitecture.Entities/Abstractions/Entity.cs
+++ NTierArchitecture.Entities/Abstractions/Entity.cs
@@ -2,5 +2,10 @@
 public abstract class Entity 
 {
     public int Id { get; set; }
-    
+    public int CreatedUserId { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int LastUpdatedUserId { get; set; }
+    public DateTime LastUpdatedDate { get; set; }
+    public bool Status { get; set; }
+    public bool isDeleted { get; set; }
 }
