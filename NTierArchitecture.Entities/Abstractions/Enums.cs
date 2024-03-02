630f0563b00050558faf46c270a807e526988046
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-02 04:12:35 +0300

    Enum aded.

---
+++ NTierArchitecture.Entities/Abstractions/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Entities.Abstractions
+{
+    public enum BodyInfo
+    {
+        S=1, M=2, L=3, Xl=4
+    }
+}
