c6ae2ffc2ee7d254db2632f7af9df2e082f9a04c
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-02 04:11:40 +0300

    ENUM EKLENDİ..

---
+++ NTierArchitecture.DataAccess/Migrations/20240302010243_denemesdsdsd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTierArchitecture.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class denemesdsdsd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Roles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Roles");
+        }
+    }
+}
--- NTierArchitecture.Entities/obj/Debug/net8.0/NTierArchitecture.Entities.AssemblyInfo.cs
+++ NTierArchitecture.Entities/obj/Debug/net8.0/NTierArchitecture.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTierArchitecture.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da6ebe8178d24ccc3bf14a3e2a9ed6290431731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad53100ba43e3f624949fa89a0fa7b14382382c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTierArchitecture.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTierArchitecture.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
