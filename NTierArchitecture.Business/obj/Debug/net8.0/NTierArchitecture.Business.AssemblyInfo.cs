50183bfc2301c06f527db973a57a7663be55804a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-10 12:03:50 +0300

    dddddd

--- NTierArchitecture.Business/obj/Debug/net8.0/NTierArchitecture.Business.AssemblyInfo.cs
+++ NTierArchitecture.Business/obj/Debug/net8.0/NTierArchitecture.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTierArchitecture.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab6d30f5812cf5b5327bf38e59aabac0f21ba1c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd50e8df066a7fb42f53847b5a523842472aa67")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTierArchitecture.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTierArchitecture.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
